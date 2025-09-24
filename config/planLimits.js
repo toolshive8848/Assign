01049bc88f1b281caa96979e55ea3fdcb91f70f3
toolshive8848 <hello.toolshive@gmail.com>
2025-09-24 13:55:48 +0400

    Create planLimits.js

---
+++ config/planLimits.js
@@ -0,0 +1,13 @@
+// config/planLimits.js
+
+module.exports = {
+  FREEMIUM: {
+    MONTHLY_CREDITS: 200
+  },
+  PRO: {
+    MONTHLY_CREDITS: 2000
+  },
+  CUSTOM: {
+    MONTHLY_CREDITS: null // unlimited or contract defined
+  }
+};
